a71a92ade44bd0db36f60bf0b687bc58b78a5be2
Thorben Schröder <stillepost@gmail.com>
2015-01-28 12:32:16 +0100

    Attempt to enable a twitter player card

--- config.rb
+++ config.rb
@@ -24,6 +24,8 @@
 #   page "/admin/*"
 # end
 
+page 'twitter-player.html', layout: false
+
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
